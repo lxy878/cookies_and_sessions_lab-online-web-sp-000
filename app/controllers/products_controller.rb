6cb57fcffdcf335a1e1c156fceed402c22aba314
Xiaoyi Liu <lxy878@gmail.com>
2020-07-10 15:24:40 +0000

    Automatically backed up by Learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,12 @@
+class ProductsController < ApplicationController
+  def index
+    @items = cart
+  end
+
+  def add
+    new_cart = cart
+    new_cart << params[:product]
+    session[:cart] = new_cart
+    render :index
+  end
+end
